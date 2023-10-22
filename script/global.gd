9aa149a577af7e9c326821fc9e7defa7f2a7f1ec
quadiez <dduidokdoi@gmail.com>
2023-10-22 12:08:18 +0700

    feat: sync song

--- script/global.gd
+++ script/global.gd
@@ -1,3 +1,10 @@
 extends Node
 
 var player_health = 5
+var score = 0
+var total_bullet = 0
+var total_bullet_black = 0
+var total_bullet_white = 0
+var total_bullet_hit = 0
+var total_bullet_hit_black = 0
+var total_bullet_hit_white = 0
--- script/main_char.gd
+++ script/main_char.gd
@@ -11,6 +11,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	_animated_sprite.play("default")
+	if !_animated_sprite.flip_h: 
+		_wheel.rotation_degrees += 0.2
+	else:
+		_wheel.rotation_degrees -= 0.2
 	
 	if Input.is_action_pressed("ui_right"):
 		_wheel.rotation_degrees += 0.5
@@ -22,12 +26,15 @@ func _process(delta):
 
 func _on_reflector_black_area_entered(area):
 	area.hit = true
+	if (area.color == "black"): area.score = 10
+	else: area.score = 5
 
 func _on_reflector_white_area_entered(area):
 	area.hit = true
+	if (area.color == "white"): area.score = 10
+	else: area.score = 5
 		
 func _on_area_2d_area_entered(area):
 	Global.player_health -= 1
 	print(Global.player_health)
-	area.queue_free()
-	pass # Replace with function body.
+	area.miss = true
